53bdc4cafb4300dd2ed01aeb5fc5a83374766f5f
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-08 14:01:12 +0300

    5th ex solution

--- ex5.py
+++ ex5.py
@@ -7,8 +7,16 @@
 # премии в качестве значения.
 # ✔ Сумма рассчитывается как ставка умноженная на процент премии.
 
+def calc_bonus(names: list, rates: list, bonus_rate: list):
+    dict_bonuses: dict = dict()
+    for name, rate, bonus in zip(names, rates, bonus_rate):
+        bonus = bonus.replace('%', '')
+        dict_bonuses[name] = rate * float(bonus) / 100
+    return dict_bonuses
 
-lst_names = ['Pifya', 'Smith', 'Neo']
-lst_interest_rate = [10, 11, 69]
-lst_bonus = ['10.50', '34.44', '69.96']
 
+lst_names: list = ['Pifya', 'Smith', 'Neo']
+lst_rate: list = [10, 11, 69]
+lst_bonus_rate: list = ['10.50%', '34.44%', '69.96%']
+bunuses = calc_bonus(lst_names, lst_rate, lst_bonus_rate)
+print(f'Here goes the money {bunuses = }')
